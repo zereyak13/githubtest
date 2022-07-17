137af1ab5398d3f092f20397342920dd810e2491
Taha Zereyak <tahazryk@gmail.com>
2022-07-18 00:04:08 +0300

    bugfix

--- gitLogic.py
+++ gitLogic.py
@@ -1,9 +1,3 @@
-localText = "Localdeki veriler"
-stageText = "2"
-version = ""
-gitRepo = []
-head = ""
-val = 0
 
 
 class GitHUB:
@@ -12,29 +6,47 @@ class GitHUB:
     ersiyon push olunca listeye yeni versiyonu ekleyecek
     head değişkeni olacak güncel versiyonu gösterek
     checkout adlı bir method olacak head de bulunan versiyonalrı değiştirecek."""
+    localText = "Localdeki veriler"
+    stageText = "2"
+    version = ""
+    gitRepo = []
+    head = ""
+    val = 0
 
     def localChanges(self):
-        localText = input("locale değişiklikleriniz ekleyiniz.")
+        self.localText = input("locale değişiklikleriniz ekleyiniz.")
 
     def git_add(self):
-        stageText = localText
+        self.stageText = self.localText
 
     def my_commit(self):
-        version = stageText
+        self.version = self.stageText
 
     def git_push(self):
-        gitRepo.append(version)
+        self.gitRepo.append(self.version)
 
-    def checkOut(versionNum):
-        head = gitRepo[versionNum]
-    while(val == 0):
-        localChanges()
-        git_add()
-        my_commit()
-        git_push()
-        versionCh = int(input("Versiyon değiştirmek ister misiniz y=0 "))
-        if(versionCh == 0):
-            versionNum = int(input("Gidilecek versiyon numarasını giriniz"))
-            checkOut(versionNum)
-            print(head)
-        val = int(input("Değişikliklere devam etmek ister misiniz? y=0"))
+    def checkOut(self, versionNum):
+        self.head = self.gitRepo[versionNum]
+
+    def startGit(self):
+        while(self.val == 0):
+            self.localChanges()
+            self.git_add()
+            self.my_commit()
+            self.git_push()
+            versionCh = int(input("Versiyon değiştirmek ister misiniz y=0 "))
+            if(versionCh == 0):
+                versionNum = int(
+                    input("Gidilecek versiyon numarasını giriniz"))
+                self.checkOut(versionNum)
+                print("verison: " + str(versionNum) + self.head)
+            self.val = int(
+                input("Değişikliklere devam etmek ister misiniz? y=0"))
+            if(self.val != 0):
+                for i in range(len(self.gitRepo)):
+                    print("version {0}: içerik:{1} ".format(
+                        i, self.gitRepo[i]))
+
+
+accesGit = GitHUB()
+accesGit.startGit()
